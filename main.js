f675c2b3f88e96f7407823fd2f57357a16060ee9
SonMyungGyoon <112740739+SonMyungGyoon@users.noreply.github.com>
2023-05-18 17:39:38 +0900

    add timer
    add timer

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ var input_pw;
 var reg_gender;
 var gender = "";
 var clock_time;
-
+var timebox;
 window.onload = function () {
   input_id = document.getElementById("input_id");
   input_pw = document.getElementById("input_pw");
   clock_time = document.getElementById("clock_box");
+  timebox = document.getElementById("timer-time");
 
   init();
 };
@@ -39,4 +40,44 @@ function user_login() {
   } else {
     alert("로그인 실패..");
   }
+}
+var timer_sec;
+var now_min;
+var now_sec;
+function timer() {
+  timer_sec = 240;
+  timebox.innerHTML = sectoMinSec(timer_sec);
+  setInterval(x, 1000);
+}
+function x() {
+  timer_sec = timer_sec - 1;
+  // timebox.innerHTML = now_min + " : " + now_sec;
+  if (timer_sec <= 0) {
+    timebox.innerHTML = "라면 불 꺼!";
+  } else {
+    timebox.innerHTML = sectoMinSec(timer_sec);
+  }
+}
+function sectoMinSec(sec) {
+  var min_var = "";
+  var sec_var = "";
+  if (parseInt(sec / 60) < 10) {
+    if (parseInt(sec / 60) == 0) {
+      min_var = "00";
+    } else {
+      min_var = "0" + parseInt(sec / 60);
+    }
+  } else {
+    min_var = parseInt(sec / 60);
+  }
+  if (sec % 60 < 10) {
+    if (sec % 60 == 0) {
+      sec_var = "00";
+    } else {
+      sec_var = "0" + (sec % 60);
+    }
+  } else {
+    sec_var = sec % 60;
+  }
+  return min_var + " : " + sec_var;
 }
